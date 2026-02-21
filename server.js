ba27024bbc53b20ffc7aad29d908555bf60e464b
a-mhamdi <a_mhamdi@outloook.com>
2026-02-21 23:23:17 +0100

    server side

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const path = require('path');
+
+const Database = require('better-sqlite3');
+
+const db = new Database('mydb.sqlite');
+
+// Create a table (runs once)
+db.exec(`CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    specialty TEXT,
+    score NUMBER)`);
+
+const app = express();
+app.use(express.json()); // Middleware to parse JSON bodies
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.status(200).send('HTML rendered successfully!');
+});
+
+app.get('/users', (req, res) => {
+    const users = db.prepare(`SELECT * FROM users`).all();
+    res.json(users);
+})
+
+app.post('/users', (req, res) => {
+    const { name, specialty, score } = req.body;
+    db.prepare(`INSERT INTO users (name, specialty, score) VALUES (?, ?, ?)`).run(name, specialty, score);
+    res.json({ message: 'Data saved successfully!' });
+});
+
+app.delete('/users', (req, res) => {
+    db.prepare('DELETE FROM users').run();
+    res.json({ message: 'All records cleared' });
+});
+
+app.listen(3000, () => {
+    console.log(`Server is running on http://localhost:3000`);
+});
